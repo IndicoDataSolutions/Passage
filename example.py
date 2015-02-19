2834a455e6d57d98bf8ec5965ef67ba57f868d2f
Grzegorz Chrupała <pitekus@gmail.com>
2015-02-19 14:42:06 +0100

    Revert "example"
    This reverts commit 2d0ff54764049c362e5d1c8b798cdcbc363c2606.

--- example.py
+++ example.py
@@ -1,24 +1,61 @@
+import os
+
+import pandas as pd
+from sklearn import metrics
+
 from passage.preprocessing import Tokenizer
 from passage.layers import Embedding, GatedRecurrent, Dense
-from passage.model import RNN
+from passage.models import RNN
+from passage.utils import load, save
 
+def load_gender_data(ntrain=10000, ntest=10000):
+    file_loc = os.path.dirname(os.path.realpath(__file__))
+    relative_path = "data/blogger_data_2.csv"
+    fullpath = os.path.join(file_loc, relative_path)
+    data = pd.read_csv(fullpath, nrows=ntrain+ntest)
+    X = data['text'].values
+    X = [str(x) for x in X] #ugly nan cleaner
+    Y = data['gender'].values
+    trX = X[:-ntest]
+    teX = X[-ntest:]
+    trY = Y[:-ntest]
+    teY = Y[-ntest:]
+    return trX, teX, trY, teY
 
-train_text = ["this is good", "quite good", "it's bad", "not too bad"]
-train_labels = [1,               1,            0,         1            ]
-test_text = ["rather good", "quite bad"]
+trX, teX, trY, teY = load_gender_data(ntrain=10000) #Can increase up to 250K or so
 
-tokenizer = Tokenizer()
-train_tokens = tokenizer.fit_transform(train_text)
+tokenizer = Tokenizer(min_df=10, max_features=50000)
+print trX[1:2] #see a blog example
+trX = tokenizer.fit_transform(trX)
+teX = tokenizer.transform(teX)
+print tokenizer.inverse_transform(trX[1:2]) #see what words are kept
+print tokenizer.n_features
 
 layers = [
     Embedding(size=128, n_features=tokenizer.n_features),
-    GatedRecurrent(size=128),
-    Dense(size=1, activation='sigmoid')
+    GatedRecurrent(size=256, activation='tanh', gate_activation='steeper_sigmoid', init='orthogonal', seq_output=False),
+    Dense(size=1, activation='sigmoid', init='orthogonal') #sigmoid for binary classification
 ]
 
-model = RNN(layers=layers, cost='BinaryCrossEntropy')
+model = RNN(layers=layers, cost='bce') #bce is classification loss for binary classification and sigmoid output
+for i in range(2):
+    model.fit(trX, trY, n_epochs=1)
+    tr_preds = model.predict(trX[:len(teY)])
+    te_preds = model.predict(teX)
 
+    tr_acc = metrics.accuracy_score(trY[:len(teY)], tr_preds > 0.5)
+    te_acc = metrics.accuracy_score(teY, te_preds > 0.5)
 
-model.fit(train_tokens, train_labels)
+    print i, tr_acc, te_acc
 
-print model.predict(tokenizer.transform(test_text))
+save(model, 'save_test.pkl') # How to save
+
+model = load('save_test.pkl') # How to load
+
+tr_preds = model.predict(trX[:len(teY)])
+te_preds = model.predict(teX)
+
+tr_acc = metrics.accuracy_score(trY[:len(teY)], tr_preds > 0.5)
+te_acc = metrics.accuracy_score(teY, te_preds > 0.5)
+
+print tr_acc, te_acc #should be same scores as above
